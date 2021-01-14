9ae3dd66542ab32d6279aaca2ebe46c8cec6fb61
nmvilla15 <niki.villalpando@gmail.com>
2021-01-14 14:54:56 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log('Hello Webpack Project.');
+
+  let button = document.querySelector("button");
+  button.addEventListener("click", function (event) {
+    alert("Button clicked!");
+  });
